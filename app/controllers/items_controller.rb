0d56ccc4ecd3543364109f76fa927966fdd4c1a2
hachi0109 <honeywisteria0109@gmail.com>
2025-12-10 18:13:28 +0900

    ログインしている場合のみ出品ページへ遷移する機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  private
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
