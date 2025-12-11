ee6064ea482793a0dab1fff0721ba56d00813754
hachi0109 <honeywisteria0109@gmail.com>
2025-12-11 14:13:58 +0900

    Activehashファイルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,26 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   #has_one :order
-  belongs_to_active_hash :category
-  belongs_to_active_hash :status
-  belongs_to_active_hash :shipping_cost
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :shipping_date
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :shipping_cost_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :shipping_date_id, numericality: { other_than: 1 }
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
 end
