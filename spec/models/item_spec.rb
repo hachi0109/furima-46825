b8e66429d53d362c2068ed523b5968ba2a4c7892
hachi0109 <honeywisteria0109@gmail.com>
2025-12-11 17:35:46 +0900

    itemモデル単体テストの実装

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品した商品の保存' do
+    context '商品を出品できる場合' do
+      it '画像、商品名、説明、カテゴリー、状態、配送料の負担、発送元地域、日数、価格を入れると出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品を出品できない場合' do
+      it '画像が空では投稿できない' do
+        @item.image = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end  
+      it '商品名が空では投稿できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end     
+      it '説明が空では投稿できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'カテゴリーが空（---）では投稿できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '状態が空（---）では投稿できない' do
+        @item.status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '配送料の負担が空（---）では投稿できない' do
+        @item.shipping_cost_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+      end
+      it '発送元地域が空（---）では投稿できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数が空（---）では投稿できない' do
+        @item.shipping_date_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank")
+      end
+      it '価格が空では投稿できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が300円未満では投稿できない' do
+        @item.price = Faker::Number.between(from: 1, to: 299)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格が9,999,999円より高くては投稿できない' do
+        @item.price = Faker::Number.between(from: 10_000_000, to: 300_000_000)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
+end
