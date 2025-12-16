f8aafef1386e2b70149d4e0d998b678a4a115e6c
hachi0109 <honeywisteria0109@gmail.com>
2025-12-16 17:40:01 +0900

    クレジット決済機能の導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,12 +3,14 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -20,12 +22,21 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :town, :street, :building, :phone).merge(
-      user_id: current_user.id, item_id: params[:item_id]
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,11 +1,11 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :town, :street, :building, :phone, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :town, :street, :building, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :town, :street
+    validates :token, :user_id, :item_id, :town, :street
     validates :post_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :phone, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
+    validates :phone, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is too short' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
   end
 
